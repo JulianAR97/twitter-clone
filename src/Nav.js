9c70a0ba13914018e40e90f44ef5025c4b75a164
Julian <julianrosenthal@yahoo.com>
2021-06-03 10:47:57 -0700

    added useEffect to handle resize

--- src/Nav.js
+++ src/Nav.js
@@ -1,15 +1,37 @@
+import navData from './data/navData'
+import React, {useState, useEffect} from 'react'
+
+const getWidth = () => window.innerWidth >= 1200 ? 'xl' : 'xs' 
+
 const Nav = () => {
+  let [width, setWidth] = useState(getWidth());
+
+  useEffect(() => {
+    let timeoutId = null;
+    const resizeListener = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => setWidth(getWidth()), 150 )
+      
+    }
+
+    window.addEventListener('resize', resizeListener);
+
+    return () => {
+      window.removeEventListener('resize', resizeListener)
+    }
+  })
+  console.log(width)
   return (
     <div id="nav">
       <ul id="nav-list">
-        <li>Home</li><br/>
-        <li>Explore</li><br/>
-        <li>Notifications</li><br/>
-        <li>Messages</li><br/>
-        <li>Bookmarks</li><br/>
-        <li>Lists</li><br/>
-        <li>Profile</li><br/>
-        <li>More</li><br/>
+        <li>{navData.home.xl}</li><br/>
+        <li>{navData.explore.xl}</li><br/>
+        <li>{navData.notifications.xl}</li><br/>
+        <li>{navData.messages.xl}</li><br/>
+        <li>{navData.bookmarks.xl}</li><br/>
+        <li>{navData.lists.xl}</li><br/>
+        <li>{navData.profile.xl}</li><br/>
+        <li>{navData.more.xl}</li><br/>
         
       </ul>
       <div id="tweet-button">Tweet</div>
