52ca24eff935b20e4a1838b9333d85618717a631
Julian <julianrosenthal@yahoo.com>
2021-06-03 13:22:49 -0700

    added navData to be imported by nav component

---
+++ src/data/navData.js
@@ -0,0 +1,14 @@
+const navData = {
+
+  home: {xl: 'Home', xs: 'h'},
+  explore: {xl: 'Explore', xs: 'e'},
+  notifications: {xl: 'Notifications', xs: 'n'},
+  messages: {xl: 'Messages', xs: 'm'},
+  bookmarks: {xl: 'Bookmarks', xs: 'b'},
+  lists: {xl: 'Lists', xs: 'l'},
+  profile: {xl: 'Profile', xs: 'p'},
+  more: {xl: 'More', xs: 'm'},
+  tweet: {xl: 'Tweet', xs: 't'}
+}
+
+export default navData
