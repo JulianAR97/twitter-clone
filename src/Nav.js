f431bb3d1360119b049ab42100088683da4dd0a2
Julian <julianrosenthal@yahoo.com>
2021-06-03 09:00:56 -0700

    added nav and began styling

---
+++ src/Nav.js
@@ -0,0 +1,20 @@
+const Nav = () => {
+  return (
+    <div id="nav">
+      <ul id="nav-list">
+        <li>Home</li><br/>
+        <li>Explore</li><br/>
+        <li>Notifications</li><br/>
+        <li>Messages</li><br/>
+        <li>Bookmarks</li><br/>
+        <li>Lists</li><br/>
+        <li>Profile</li><br/>
+        <li>More</li><br/>
+        
+      </ul>
+      <div id="tweet-button">Tweet</div>
+    </div>
+  )
+}
+
+export default Nav
